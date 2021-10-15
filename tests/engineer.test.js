0e5e3e6701997e27853ecfd29e0663285a364ad4
Jordan <jordansoh14@gmail.com>
2021-10-15 14:01:12 -0500

    added engineer test

---
+++ tests/engineer.test.js
@@ -0,0 +1,31 @@
+const Engineer = require("../lib/Engineer");
+
+it('able to set an engineers name throught the contructor ', () => {
+    const testValue = "engineer Name"
+    const engineer = new Engineer(testValue)
+    expect(engineer.name).toBe(testValue)
+});
+
+it('able to set an engineers name throught the contructor ', () => {
+const testValue = "engineer Name"
+const idValue = 1234
+const engineer = new Engineer(testValue, idValue)
+expect(engineer.id).toBe(idValue)
+});
+
+it('able to set an engineers name throught the contructor ', () => {
+const testValue = "engineer Name"
+const idValue = 1234
+const emailValue = "email@email.com"
+const engineer = new Engineer(testValue, idValue, emailValue)
+expect(engineer.email).toBe(emailValue)
+});
+
+it('able to set an engineers name throught the contructor ', () => {
+const testValue = "engineer Name"
+const idValue = 1234
+const emailValue = "email@email.com"
+const gitValue = "https://github.com/user"
+const engineer = new Engineer(testValue, idValue, emailValue, gitValue)
+expect(engineer.github).toBe(gitValue)
+});
